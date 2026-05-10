9ad94871208593c2267e6dee7308a1fc39263761
Marcos Gabriel <mgabriel.dev937@gmail.com>
2026-05-10 09:01:40 -0300

    Subi todas as tabelas já criadas e adicionei a classe main para o código dar run no código

--- src/main/java/com/example/tabela_especialistas/Controller/EspecialistaController.java
+++ src/main/java/com/example/tabela_especialistas/Controller/EspecialistaController.java
@@ -1,4 +1,27 @@
 package com.example.tabela_especialistas.Controller;
+// atualizando o "Controller" para poder jogar no post e ver como se comporta no get.
+import com.example.tabela_especialistas.especialista.DadosCadastroEspecialista;
+import com.example.tabela_especialistas.especialista.Especialista;
+import com.example.tabela_especialistas.especialista.EspecialistaRepository;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@RestController
+@RequestMapping("/especialistas")
 public class EspecialistaController {
+
+    private final EspecialistaRepository repository;
+
+    public EspecialistaController(EspecialistaRepository repository) {
+        this.repository = repository;
+    }
+    @PostMapping
+    public void cadastro(@RequestBody DadosCadastroEspecialista dados){
+        repository.save(new Especialista(dados));
+    }
+
+    @GetMapping
+    public List<Especialista> listar() {
+        return repository.findAll();
+    }
 }
